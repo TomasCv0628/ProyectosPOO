073e5fb42e664ea861be9cb407f8569ef6af0ace
TomasCv0628 <tomas2224508@correo.uis.edu.co>
2025-09-22 10:55:23 -0500

    Squashed 'CasoBiblioteca/' content from commit 9d195df
    git-subtree-dir: CasoBiblioteca
    git-subtree-split: 9d195dfd43c648a6bd22c559f67c3799ba58db13

---
+++ vista/VentanaPrincipal.java
@@ -0,0 +1,60 @@
+package CasoBiblioteca.vista;
+
+import javax.swing.JFrame;
+
+public class VentanaPrincipal extends JFrame
+{
+    //----------------------
+    // Atributos
+    //----------------------
+    public PanelEntradaDatos miPanelEntradaDatos;
+    public PanelOperaciones miPanelOperaciones;
+    public PanelResultados miPanelResultados;
+    public DialogoAgregarAColeccion miColeccion;
+    public DialogoAgregarLibro miLibro;
+
+    
+    //----------------------
+    // Metodos
+    //----------------------
+
+    //Constructor
+    public VentanaPrincipal()
+    {
+        //Definición del layout de la ventana
+        this.setLayout(null);
+
+        //Creación y adición del PanelEntradaDatos
+        miPanelEntradaDatos = new PanelEntradaDatos();
+        miPanelEntradaDatos.setBounds(10,10,380,190);
+        this.add(miPanelEntradaDatos);
+
+        //Creación y adición del PanelOperaciones
+        miPanelOperaciones = new PanelOperaciones();
+        miPanelOperaciones.setBounds(10,220,380,120);
+        this.add(miPanelOperaciones);
+
+        //Creación y adición del PanelResultados
+        miPanelResultados = new PanelResultados();
+        miPanelResultados.setBounds(10,350,380,190);
+        this.add(miPanelResultados);
+
+        //Caracteristicas de la ventana
+        this.setTitle("Empresa Automotriz");
+        this.setSize(400,600);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
+    public void crearDialogoAgregarColeccion()
+    {
+        miColeccion = new DialogoAgregarAColeccion();
+    }
+
+    //Crear Dialogo VenderCarro
+    public void crearDialogoAgregarLibro()
+    {
+        miLibro = new DialogoAgregarLibro();
+    }
+}
